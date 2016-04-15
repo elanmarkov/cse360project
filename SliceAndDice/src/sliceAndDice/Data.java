20ffd2c089e370d9e7d6e0c02e53508f250d018c
Andrew Stanton <dstanto2@asu.edu>
2016-04-15 13:31:33 -0700

    Change Scoreboard name and added get and set to Data for rank and score

--- SliceAndDice/src/sliceAndDice/Data.java
+++ SliceAndDice/src/sliceAndDice/Data.java
@@ -3,6 +3,8 @@ package sliceAndDice;
 import sliceAndDice.Data;
 
 public class Data {
+	double score;
+	int rank;
 	int totalGameCount;
 	int totalWinCount;
 	//int totalNumTurns;
@@ -15,125 +17,141 @@ public class Data {
 	// float totalGameTime;
 	// float totalTurnTime;
 
-Data() {
-totalGameCount = 0;
-totalWinCount = 0;
-//totalNumTurns = 0;
-totalNumAttacks = 0;
-totalNumMeals = 0;
-// totalNumSPAttacks = 0;
-totalHealthLost = 0;
-totalManaUsed = 0;
-totalFoodUsed = 0;
-// totalGameTime = 0;
-// totalTurnTime = 0;
-}
-void loadData(Data data) {
-totalGameCount = data.getGame();
-totalWinCount = data.getWin();
-//totalNumTurns = data.getTurn();
-totalNumAttacks = data.getAttack();
-totalNumMeals = data.getMeal();
-// totalNumSPAttacks = data.getSPAttack();
-totalHealthLost = data.getHPLost();
-totalManaUsed = data.getManaUsed();
-totalFoodUsed = data.getFoodUsed();
-// totalGameTime = data.getGameTime();
-// totalTurnTime = data.getTurnTime();
-}
-/* set methods */
-void setGame(int totalGameCount) {
-	this.totalGameCount = totalGameCount;
-}
-void setWin(int totalWinCount) {
-	this.totalWinCount = totalWinCount;
-}
-
-//void setTurn(int totalNumTurns) {
-//	this.totalNumTurns = totalNumTurns;
-//}
-
-void setAttack(int totalNumAttacks) {
-	this.totalNumAttacks = totalNumAttacks;
-}
-
-void setMeal(int totalNumMeals) {
-	this.totalNumMeals = totalNumMeals;
-}
-
-void setHPLost(int totalHealthLost) {
-	this.totalHealthLost = totalHealthLost;
-}
-
-void setManaUsed(int totalManaUsed) {
-	this.totalManaUsed = totalManaUsed;
-}
-
-void setFoodUsed(int totalFoodUsed) {
-	this.totalFoodUsed = totalFoodUsed;
-}
-/* get methods */
-int getGame() {
-	return totalGameCount;
-}
-int getWin() {
-	return totalWinCount;
-}
-
-//int getTurn() {
-//	return totalNumTurns;
-//}
-
-int getAttack() {
-	return totalNumAttacks;
-}
-
-int getMeal() {
-	return totalNumMeals;
-}
-
-int getHPLost() {
-	return totalHealthLost;
-}
-
-int getManaUsed() {
-	return totalManaUsed;
-}
-
-int getFoodUsed() {
-	return totalFoodUsed;
-}
-
-// called at end of each game
-void incrGameCount() {
-	totalGameCount++;
-}
-// called at end of each game
-public void incrWinCount() {
-	totalWinCount++;
-}
-// called at end of each turn
-//public void incrNumTurns() {
-//	totalNumTurns++;
-//}
-// called at end of each game
-public void updateHealthLost(int finalHP) {
-	totalHealthLost = 100 - finalHP; // 100 is maxHP
-}
-// called at end of each game
-public void updateManaUsed(int finalMana) {
-	totalManaUsed = 30 - finalMana; // 30 is maxMana
-}
-// called at end of each game
-public void updateFoodUsed(int finalFood) {
-	totalFoodUsed = 5 - finalFood; // 5 is maxFood
-}
-public void incrNumMeals() {
-	totalNumMeals++;
-}
-public void incrNumAttacks() {
-	totalNumAttacks++;
-}
+	Data() {
+	score = 1000;
+	rank = 0;
+	totalGameCount = 0;
+	totalWinCount = 0;
+	//totalNumTurns = 0;
+	totalNumAttacks = 0;
+	totalNumMeals = 0;
+	// totalNumSPAttacks = 0;
+	totalHealthLost = 0;
+	totalManaUsed = 0;
+	totalFoodUsed = 0;
+	// totalGameTime = 0;
+	// totalTurnTime = 0;
+	}
+	void loadData(Data data) {
+	score = data.getScore();
+	rank = data.getRank();
+	totalGameCount = data.getGame();
+	totalWinCount = data.getWin();
+	//totalNumTurns = data.getTurn();
+	totalNumAttacks = data.getAttack();
+	totalNumMeals = data.getMeal();
+	// totalNumSPAttacks = data.getSPAttack();
+	totalHealthLost = data.getHPLost();
+	totalManaUsed = data.getManaUsed();
+	totalFoodUsed = data.getFoodUsed();
+	// totalGameTime = data.getGameTime();
+	// totalTurnTime = data.getTurnTime();
+	}
+	/* set methods */
+	void setScore(double score) {
+		this.score = score;
+	}
+	void setRank(int rank) {
+		this.rank = rank;
+	}
+	void setGame(int totalGameCount) {
+		this.totalGameCount = totalGameCount;
+	}
+	void setWin(int totalWinCount) {
+		this.totalWinCount = totalWinCount;
+	}
+	
+	//void setTurn(int totalNumTurns) {
+	//	this.totalNumTurns = totalNumTurns;
+	//}
+	
+	void setAttack(int totalNumAttacks) {
+		this.totalNumAttacks = totalNumAttacks;
+	}
+	
+	void setMeal(int totalNumMeals) {
+		this.totalNumMeals = totalNumMeals;
+	}
+	
+	void setHPLost(int totalHealthLost) {
+		this.totalHealthLost = totalHealthLost;
+	}
+	
+	void setManaUsed(int totalManaUsed) {
+		this.totalManaUsed = totalManaUsed;
+	}
+	
+	void setFoodUsed(int totalFoodUsed) {
+		this.totalFoodUsed = totalFoodUsed;
+	}
+	/* get methods */
+	double getScore() {
+		return score;
+	}
+	int getRank() {
+		return rank;
+	}
+	int getGame() {
+		return totalGameCount;
+	}
+	int getWin() {
+		return totalWinCount;
+	}
+	
+	//int getTurn() {
+	//	return totalNumTurns;
+	//}
+	
+	int getAttack() {
+		return totalNumAttacks;
+	}
+	
+	int getMeal() {
+		return totalNumMeals;
+	}
+	
+	int getHPLost() {
+		return totalHealthLost;
+	}
+	
+	int getManaUsed() {
+		return totalManaUsed;
+	}
+	
+	int getFoodUsed() {
+		return totalFoodUsed;
+	}
+	
+	// called at end of each game
+	void incrGameCount() {
+		totalGameCount++;
+	}
+	// called at end of each game
+	public void incrWinCount() {
+		totalWinCount++;
+	}
+	// called at end of each turn
+	//public void incrNumTurns() {
+	//	totalNumTurns++;
+	//}
+	public void incrNumAttacks() {
+		totalNumAttacks++;
+	}
+	public void incrNumMeals() {
+		totalNumMeals++;
+	}
+	// called at end of each game
+	public void updateHealthLost(int finalHP) {
+		totalHealthLost = 100 - finalHP; // 100 is maxHP
+	}
+	// called at end of each game
+	public void updateManaUsed(int finalMana) {
+		totalManaUsed = 30 - finalMana; // 30 is maxMana
+	}
+	// called at end of each game
+	public void updateFoodUsed(int finalFood) {
+		totalFoodUsed = 5 - finalFood; // 5 is maxFood
+	}
 }
 /* 
 public void incrNumSPAttacks() {
