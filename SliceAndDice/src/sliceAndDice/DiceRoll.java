9105d0fe4676ac95e5ad0380f471fb14dadded4e
emarkovcse360 <emarkov@asu.edu>
2016-04-15 10:32:32 -0700

    Javadoc added to Player class.

--- SliceAndDice/src/sliceAndDice/DiceRoll.java
+++ SliceAndDice/src/sliceAndDice/DiceRoll.java
@@ -1,6 +1,31 @@
 package sliceAndDice;
+/**
+ * Class to perform random dice rolls for the dice game.
+ * 
+ * @author Team 1, CSE 360 Spring 2016
+ * Group members:
+ * Jacob Loden
+ * Elan Markov
+ * Daniel Saman
+ * Eisher Saroya
+ * Andrew Stanton
+ *
+ */
 
+/**
+ * DiceRoll class.
+ * Final class with static methods, returns an array containing
+ * a specified number of rolls.
+ * Uses six-sided dice by default, can be configured to use any
+ * number of sides.
+ *
+ */
 final class DiceRoll {
+	/**
+	 * Default roll method.
+	 * @param numRoll number of rolls to perform
+	 * @return array containing the value of all random rolls performed
+	 */
 	static int[] roll(int numRoll) {
 		// Default dice roll, 6-sided dice
 		int[] rolls = new int[numRoll];
@@ -9,7 +34,12 @@ final class DiceRoll {
 		}
 		return rolls;
 	}
-	
+	/**
+	 * Roll method with a specified dice size.
+	 * @param numRoll number of rolls to perform
+	 * @param numSides how many sides the dice for this roll will have
+	 * @return array containing the value of all random rolls performed
+	 */
 	static int[] roll(int numRoll, int numSides) {
 		// Specify any x-sided dice
 		int[] rolls = new int[numRoll];
--- SliceAndDice/src/sliceAndDice/Player.java
+++ SliceAndDice/src/sliceAndDice/Player.java
@@ -1,5 +1,18 @@
 package sliceAndDice;
-
+/**
+ * Player class to store information about each player, 
+ * including username, ID, and game statistics.
+ * Status class to store player status within a game.
+ * 
+ * @author Team 1, CSE 360 Spring 2016
+ * Group members:
+ * Jacob Loden
+ * Elan Markov
+ * Daniel Saman
+ * Eisher Saroya
+ * Andrew Stanton
+ *
+ */
 import sliceAndDice.Condition;
 import sliceAndDice.Move;
 import sliceAndDice.Status;
@@ -7,27 +20,61 @@ import sliceAndDice.Status;
 enum Move {ATTACK, FOOD, FREEZE, DOUBLEATK, SPATK3, SPATK4};
 enum Condition {NONE}
 
+/**
+ * Player class. Stores identifying information (username and 
+ * unique ID) along with statistical data (Data object) for each
+ * player.
+ *
+ */
 public class Player {
 	String username;
 	int playerID;
 	Data playerData;
+	/**
+	 * Constructor for player class.
+	 * @param username player's(unique) username
+	 * @param playerID unique ID for player
+	 */
 	Player(String username, int playerID) {
 		this.username = username;
 		this.playerID = playerID;
 		playerData = new Data();
 	}
+	/**
+	 * Setter class for ID.
+	 * @param playerID ID value; corresponds to player location in storage, integer.
+	 */
 	void setID(int playerID) {
 		this.playerID = playerID;
 	}
+	/**
+	 * Getter class for ID.
+	 * @return User's unique ID, integer.
+	 */
 	int getID() {
 		return playerID;
 	}
-	String getUsername() {
-		return username;
-	}
+	/**
+	 * Setter class for player's username.
+	 * @param username Player's username, String.
+	 */
 	void setUsername(String username) {
 		this.username = username;
 	}
+	/**
+	 * Getter class for player's username.
+	 * @return Player's username, String.
+	 */
+	String getUsername() {
+		return username;
+	}
+	void setPlayerData(Data playerData) {
+		this.playerData =  playerData;
+	}
+	/**
+	 * Getter class for user's data statistics.
+	 * @return Data object containing statistics.
+	 */
 	Data getPlayerData() {
 		return playerData;
 	}
