c364bc88301dc31842d8d40e22f1ec15cccffd8b
Elan Markov <emarkov@asu.edu>
2016-04-15 23:12:23 -0700

    Merge.

--- SliceAndDice/src/sliceAndDice/GameTest.java
+++ SliceAndDice/src/sliceAndDice/GameTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 public class GameTest {
 
 	@Test
-<<<<<<< HEAD
 	public void gameNotNull() {
 		// Ensure that a Game instance properly initializes
 		Player player1 = new Player();
@@ -16,14 +15,6 @@ public class GameTest {
 		assertNotNull(game);
 		
 	} 
-=======
-	public void playNextTurnTest() {
-	
-		Game game = new Game("playerone", "playertwo");
-		assertEquals(game.PlayNextTurn(Move.ATTACK), Winner.NONE);
-		
-	}
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 	
 	@Test
 	public void player1StatusNotNull() {
@@ -32,43 +23,8 @@ public class GameTest {
 		Player player2 = new Player();
 		Game game = new Game(player1, player2);	
 		
-<<<<<<< HEAD
 		assertNotNull(game.getPlayerOneStatus());
-=======
-	}
-	
-	@Test
-	public void isPlayerOneTurnTest() {
-		
-		Game game = new Game("playerone", "playertwo");
-		assertEquals(game.isPlayerOneTurn(), true);
-	}
-	
-	@Test
-	public void getPlayerOneStatusTest() {
-		
-		fail("cannot access private variables");
-	}
-	
-	@Test
-	public void getPlayerTwoStatusTest() {
-		
-		fail("cannot access private variables");
-	}
-	
-	@Test
-	public void getWinnerIDTest() {
-		
-		Game game = new Game("Playerone", "playertwo");
-		assertEquals(game.getWinnerID(), -1);
-		
-	}
-	
-	@Test
-	public void getLoserIDTest() {
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 
-<<<<<<< HEAD
 	}
 	
 	@Test
@@ -97,24 +53,14 @@ public class GameTest {
 		Game game = new Game(player1, player2);	
 		game.PlayNextTurn(Move.ATTACK);
 		assertEquals(game.isPlayerOneTurn(), false);
-=======
-		Game game = new Game("Playerone", "playertwo");
-		assertEquals(game.getLoserID(), -1);
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 	}
 	
 	@Test
 	public void getTotalTurnsTest() {
-<<<<<<< HEAD
 		Player player1 = new Player();
 		Player player2 = new Player();
 		Game game = new Game(player1, player2);	
-=======
-		Game game = new Game("playerone", "playertwo");
-		assertEquals(game.getTotalTurns(), 0);
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 		
-<<<<<<< HEAD
 		game.PlayNextTurn(Move.ATTACK);
 		game.PlayNextTurn(Move.ATTACK);
 		game.PlayNextTurn(Move.ATTACK);
@@ -123,8 +69,6 @@ public class GameTest {
 		game.PlayNextTurn(Move.ATTACK);
 		
 		assertEquals(game.getTotalTurns(), 6);
-=======
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 	}
 	
 	@Test
@@ -140,20 +84,12 @@ public class GameTest {
 	}
 	
 	@Test
-<<<<<<< HEAD
 	public void gameHasWinner() {
 		Player player1 = new Player();
 		Player player2 = new Player();
 		Game game = new Game(player1, player2);	
 		boolean isWinner = false;
 		Winner result = Winner.NONE;
-=======
-	public void updateMoveCountTest() {
-		Game game = new Game("playerone", "playertwo");
-		Player player1 = new Player();
-		game.updateMoveCount(player1, Move.ATTACK );
-		assertEquals(player1.getPlayerData().totalNumAttacks, 1);
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 		
 		int turnCount = 0;
 		while(!isWinner && turnCount < 40) {
@@ -232,11 +168,10 @@ public class GameTest {
 	
 	@Test
 	public void playNextTurnTest2() {
-		Status status1 = new Status();
-		Status status2 = new Status();		
-		Turn turn = new Turn(status1, status2);
-		turn.playNextTurn(Move.ATTACK, status1, status2);
-		assertTrue(status1.getHitPts() > status2.getHitPts());
+		
+		Game game = new Game("playerOne", "playerTwo");
+		game.nextTurn.playNextTurn(Move.ATTACK, game.getPlayerOneStatus(), game.getPlayerTwoStatus());
+		assertTrue(turn.statusP1.getHitPts() > turn.statusP2.getHitPts());
 	}
 	
 	@Test
@@ -247,103 +182,39 @@ public class GameTest {
 	@Test
 	public void moveIsLegalTest() {
 		
-		Status status1 = new Status();
-		Status status2 = new Status();
-		Turn turn = new Turn(status1, status2);
-		
-		assertEquals(turn.moveIsLegal(status1, Move.SPATK4), IllegalMove.NOTIMPLEMENTED);
 	}
 	@Test
 	public void attackTest() {
 		Status status1 = new Status();
 		Status status2 = new Status();
 		Turn turn = new Turn(status1, status2);
-		turn.attack(status1, status2);
-		assertTrue(status2.getHitPts() < status1.getHitPts());
+		turn.attack(turn.statusP1, turn.statusP2);
+		assertTrue(turn.statusP2.getHitPts() < turn.statusP1.getHitPts());
 		
 	}
 	
 	@Test
 	public void foodTest() {
-		Status status1 = new Status();
-		Status status2 = new Status();
-		Turn turn = new Turn(status1, status2);
-		turn.food(status1);
-		assertEquals(status1.getFoodCount(), status1.getMaxFood() - 1);
+		
 	}
 	
 	@Test
 	public void freezeTest() {
-
-		Status status1 = new Status();
-		Status status2 = new Status();		
-		Turn turn = new Turn(status1, status2);
-		boolean thrown = false;
 		
-		try{
-			turn.freeze(status1, status2);
-		}
-		catch(IllegalArgumentException e) {
-			thrown = true;
-		}
-		
-		assertTrue(thrown);
 	}
 	
 	@Test
-	public void doubleAtkTest() {
+	public void doubleAttackTest() {
 		
-		Status status1 = new Status();
-		Status status2 = new Status();		
-		Turn turn = new Turn(status1, status2);
-		boolean thrown = false;
-		
-		try{
-			turn.doubleAtk(status1, status2);
-		}
-		catch(IllegalArgumentException e) {
-			thrown = true;
-		}
-		
-		assertTrue(thrown);
 	}
 	
 	@Test
 	public void spAtk3Test() {
 		
-		Status status1 = new Status();
-		Status status2 = new Status();		
-		Turn turn = new Turn(status1, status2);
-		boolean thrown = false;
-		
-		try{
-			turn.spAtk3(status1, status2);
-		}
-		catch(IllegalArgumentException e) {
-			thrown = true;
-		}
-		
-		assertTrue(thrown);
 	}
 	
 	@Test
 	public void spAtk4Test() {
-		Status status1 = new Status();
-		Status status2 = new Status();		
-		Turn turn = new Turn(status1, status2);
-		boolean thrown = false;
 		
-<<<<<<< HEAD
 	}*/
-=======
-		try{
-			turn.spAtk4(status1, status2);
-		}
-		catch(IllegalArgumentException e) {
-			thrown = true;
-		}
-		
-		assertTrue(thrown);
-	}
->>>>>>> branch 'master' of https://github.com/emarkovcse360/cse360project.git
 }
